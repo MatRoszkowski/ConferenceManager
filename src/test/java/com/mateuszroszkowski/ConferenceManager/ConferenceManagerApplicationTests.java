965fdcde81c29f3b04e4807aad1daddd13912208
Mateusz Roszkowski <mateusz.roszkowski96@gmail.com>
2022-05-27 09:04:40 +0200

    init commit

---
+++ src/test/java/com/mateuszroszkowski/ConferenceManager/ConferenceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateuszroszkowski.ConferenceManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
